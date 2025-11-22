346047e78c86f6c139ae02af2ef9305d760ccb69
Hermann Afeseh Buma <156859033+hermannafesehbuma@users.noreply.github.com>
2025-11-22 06:01:22 -0500

    live chat added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Navigation } from "@/components/navigation";
 import { Footer } from "@/components/footer";
@@ -109,6 +110,25 @@ export default function RootLayout({
           {children}
           <Footer />
         </CartProvider>
+        
+        {/* Tawk.to Chat Widget */}
+        <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/692196efca4f9d19612f7d02/1jaljb6bg';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
